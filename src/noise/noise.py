714434ee06b977facafc69cfedb303bec37d1f36
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2025-05-20 18:42:58 +0300

    Add functionality to remove data based on generated masks

--- src/noise/noise.py
+++ src/noise/noise.py
@@ -1,13 +1,30 @@
 import numpy as np
 import pandas as pd
 
+##############################
+## Loading original dataset ##
+##############################
+
 df = pd.read_csv("../../data/original_student_habits_performance.csv")
 
 print(df.info())
 
+######################################
+## Creating masks for removing data ##
+######################################
+
 mask_study_hours = np.random.rand(len(df)) < 0.05
-mask_extre_activity = np.random.rand(len(df)) < 0.2
+mask_extra_activity = np.random.rand(len(df)) < 0.2
 mask_diet_quality = np.random.rand(len(df)) < 0.1
 mask_mental_health = np.random.rand(len(df)) < 0.01
 mask_age = np.random.rand(len(df)) < 0.01
-mask_sociaL_media = np.random.rand(len(df)) < 0.1
+mask_social_media = np.random.rand(len(df)) < 0.1
+
+df.loc[mask_study_hours, 'study_hours_per_day'] = np.nan
+df.loc[mask_age, 'age'] = np.nan
+df.loc[mask_diet_quality, 'diet_quality'] = np.nan
+df.loc[mask_extra_activity, 'extracurricular_participation'] = np.nan
+df.loc[mask_social_media, 'social_media_hours'] = np.nan
+df.loc[mask_mental_health, 'mental_health_rating'] = np.nan
+
+print(df.info())
