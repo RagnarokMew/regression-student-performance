b6882447f9d45f90163806d4ee7a7f14b5764cb9
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2025-05-21 17:11:15 +0300

    Add saving new df & adjust removed values

--- src/noise/noise.py
+++ src/noise/noise.py
@@ -46,8 +46,8 @@ df.loc[df['exercise_frequency'] < 0, 'exercise_frequency'] = 0
 mask_study_hours = np.random.rand(len(df)) < 0.05
 mask_extra_activity = np.random.rand(len(df)) < 0.2
 mask_diet_quality = np.random.rand(len(df)) < 0.1
-mask_mental_health = np.random.rand(len(df)) < 0.01
-mask_age = np.random.rand(len(df)) < 0.01
+mask_mental_health = np.random.rand(len(df)) < 0.15
+mask_age = np.random.rand(len(df)) < 0.05
 mask_social_media = np.random.rand(len(df)) < 0.1
 mask_netflix = np.random.rand(len(df)) < 0.25
 
@@ -68,3 +68,4 @@ df['age'] = df['age'].astype('Int64')
 df['mental_health_rating'] = df['mental_health_rating'].astype('Int64')
 
 print(df.info())
+df.to_csv("../data/noised_student_habits_performance.csv")
