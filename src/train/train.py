cde81987570ac9fc6e5d275991192ecafc1fe417
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2025-05-25 19:47:32 +0300

    Add linear regression model

---
+++ src/train/train.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+X_train = pd.read_csv("../../data/train.csv")
+y_train = X_train['exam_score']
+X_train.drop(columns='exam_score')
+
+X_test = pd.read_csv("../../data/test.csv")
+y_test = X_test['exam_score']
+X_test.drop(columns='exam_score')
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+
+print(f"Root mean squared error: {rmse}")
+
+plt.scatter(y_test, y_pred, s=2, color='red', label='Model prediction')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], '--', color='lightgrey', label='Real values')
+plt.title('Error graph')
+plt.legend()
+plt.savefig("../../raw/error_graph.png")
+plt.close()
