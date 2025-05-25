ef7a452bafe535c6e6487653f29350f5e642c15c
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2025-05-25 11:48:14 +0300

    Add missing data num and percentage

--- src/eda/missing.py
+++ src/eda/missing.py
@@ -6,4 +6,13 @@ pd.set_option('display.max_columns', None)
 csv_path = sys.argv[1]
 
 df = pd.read_csv(csv_path)
+
+print()
+
+for val in df:
+    missing = df[val].isnull().sum()
+    print(f"{val}: {missing} / {round(missing / len(df) * 100, 2)}% missing values")
+
+print()
+
 print(df.describe())
