a670543cfd13a5f2e784dcaa71ba3e353881245f
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2025-05-25 18:16:11 +0300

    Complete filling missing values script

--- src/process/fill_missing.py
+++ src/process/fill_missing.py
@@ -5,14 +5,14 @@ import pandas as pd
 csv_path = sys.argv[1]
 df = pd.read_csv(csv_path)
 
-df['age'].fillnas(df['age'].median(), inplace=True)
-df['study_hours_per_day'].fillnas(df['study_hours_per_day'].median(), inplace=True)
-df['social_media_hours'].fillnas(df['social_media_hours'].median(), inplace=True)
-df['netflix_hours'].fillnas(df['netflix_hours'].median(), inplace=True)
+df['age'].fillna(df['age'].median(), inplace=True)
+df['study_hours_per_day'].fillna(df['study_hours_per_day'].median(), inplace=True)
+df['social_media_hours'].fillna(df['social_media_hours'].median(), inplace=True)
+df['netflix_hours'].fillna(df['netflix_hours'].median(), inplace=True)
 
-df['diet_quality']
-df['parental_education_level']
-df['mental_health_rating']
-df['extracurricular_participation']
+df['diet_quality'].fillna(df['diet_quality'].mode()[0], inplace=True)
+df['parental_education_level'].fillna(df['parental_education_level'].mode()[0], inplace=True)
+df['mental_health_rating'].fillna(df['mental_health_rating'].mode()[0], inplace=True)
+df['extracurricular_participation'].fillna(df['extracurricular_participation'].mode()[0], inplace=True)
 
-df.to_csv(f"../../data/final.csv", index=False)
+df.to_csv("../../data/final.csv", index=False)
