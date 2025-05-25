e8bcaf0e4113a310468d43f3a2e11d1069353890
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2025-05-25 15:05:20 +0300

    Add correlation matrix script

---
+++ src/eda/correlation.py
@@ -0,0 +1,22 @@
+import sys
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+csv_path = sys.argv[1]
+df = pd.read_csv(csv_path)
+
+numeric = df.select_dtypes(include='number')
+corr_target = numeric.corr()
+
+plt.figure(figsize=(12, 8))
+sns.heatmap(corr_target, 
+            annot=True, 
+            fmt=".2f", 
+            cmap='Blues',
+            linewidths=0.5)
+plt.title('Correlation matrix heatmap')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.title('Correlation matrix heatmap')
+plt.savefig("../../raw/corr_heat.png")
